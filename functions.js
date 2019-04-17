ff64b95c7f7bcae3450be813ff6c085ec30fa31d
John Taylor <lewtay2597@gmail.com>
2019-04-17 22:06:52 +0100

    Update refined the generate and clear grid functions. Added grid sizing functionality to customise grid size.

--- functions.js
+++ functions.js
@@ -1,35 +1,73 @@
 // Generates the initial 16 x 16 grid.
-function generateGrid() {
 
-    let grid = 16*16;
+function init(grid) {
+
+    
 
     let i = 0;
-
+    let cell = "";
+    let container = "";
+ 
     do {
+      let cell = document.createElement('div');
+      let container = document.getElementById('grid-container');
 
-        let cell = document.createElement('div');
-        let container = document.getElementById('grid-container');
+      cell.setAttribute('class', 'grid-item');
+      cell.setAttribute('id', i);
 
-        cell.setAttribute('class', 'grid-item');
-        cell.setAttribute('id',i);
+      cell.setAttribute('onmouseover', 'colorCell(id)');
+      container.appendChild(cell);
 
-        cell.setAttribute('onmouseover','colorCell(id)');
-        container.appendChild(cell)
+      container.style.gridTemplateColumns = `repeat(${grid}, 1fr)`;
+      container.style.gridTemplateRows = `repeat(${grid}, 1fr)`;
 
-        i++;
+      i++;
+    } while (i != grid * grid);
 
-    } while (i != grid);
+    i = 0;
+}
+
+function generateGrid(resize) {
+
+    clearGrid();
+    
+    let i = 0;
+    let cell = "";
+    let container = "";
+
+    if (resize == true) {
+        grid = parseInt(prompt("Please enter a grid size."));
+    }
+ 
+    do {
+      let cell = document.createElement('div');
+      let container = document.getElementById('grid-container');
+
+      cell.setAttribute('class', 'grid-item');
+      cell.setAttribute('id', i);
+
+      cell.setAttribute('onmouseover', 'colorCell(id)');
+      container.appendChild(cell);
+
+      container.style.gridTemplateColumns = `repeat(${grid}, 1fr)`;
+      container.style.gridTemplateRows = `repeat(${grid}, 1fr)`;
+
+      i++;
+    } while (i != grid * grid);
+
+    resize = false;
+  
+    
 }
 
 function colorCell(id) {
-
-    let currentCell = document.getElementById(id.toString());
-    currentCell.setAttribute('style', 'background-color: black;');
+  let currentCell = document.getElementById(id.toString());
+  currentCell.setAttribute('style', 'background-color: black;');
 }
 
 function clearGrid() {
-    let grid = document.getElementsByClassName('grid-item');
-    for(var i = 0; i < grid.length; i++){
-        grid[i].setAttribute('style', 'background-color: white;');    }
-    
+  let grid = document.getElementById('grid-container');
+  var range = document.createRange();
+  range.selectNodeContents(grid);
+  range.deleteContents();
 }
