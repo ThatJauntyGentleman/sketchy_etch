60029277c991460503acef4f7a2cc3acf3549c27
John Taylor <lewtay2597@gmail.com>
2019-04-17 18:08:17 +0100

    Update completed code to generate the initial 16x16 grid.

--- functions.js
+++ functions.js
@@ -1,3 +1,25 @@
+// Generates the initial 16 x 16 grid.
 function generateGrid() {
-    
+
+    let grid = 16*16;
+
+    let i = 0;
+
+    do {
+
+        let cell = document.createElement('div');
+        let container = document.getElementById('grid-container');
+
+        cell.setAttribute('class', 'grid-item');
+        cell.setAttribute('id',i);
+
+        cell.setAttribute('onmouseover','colorCell(id)');
+        container.appendChild(cell)
+
+        i++;
+
+    } while (i != grid);
+
+
 }
+
