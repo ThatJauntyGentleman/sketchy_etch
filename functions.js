2a80a2816a36b9cb2145d04f73b78e9a4d1b6589
John Taylor <lewtay2597@gmail.com>
2019-04-22 18:48:32 +0100

    Update finished functions for changing colors. Amended code to have event listeners in functions.js file instead of inside index.html.

--- functions.js
+++ functions.js
@@ -1,73 +1,158 @@
 // Generates the initial 16 x 16 grid.
 
-function init(grid) {
+(function init() {
+  let i = 0;
+  let gridSize = 16;
 
-    
+  do {
+    let cell = document.createElement('div');
+    let container = document.getElementById('grid-container');
 
-    let i = 0;
-    let cell = "";
-    let container = "";
- 
-    do {
-      let cell = document.createElement('div');
-      let container = document.getElementById('grid-container');
+    cell.setAttribute('class', 'grid-item');
+    cell.setAttribute('id', i);
 
-      cell.setAttribute('class', 'grid-item');
-      cell.setAttribute('id', i);
+    cell.setAttribute('onmouseover', 'monochromeMode(id)');
+    container.appendChild(cell);
 
-      cell.setAttribute('onmouseover', 'colorCell(id)');
-      container.appendChild(cell);
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
-      container.style.gridTemplateColumns = `repeat(${grid}, 1fr)`;
-      container.style.gridTemplateRows = `repeat(${grid}, 1fr)`;
+    i++;
+  } while (i != gridSize * gridSize);
 
-      i++;
-    } while (i != grid * grid);
+  //Adds event listeners
 
-    i = 0;
-}
+  let btnClearGrid = document.getElementById('clearGrid');
 
-function generateGrid(resize) {
-
+  btnClearGrid.addEventListener('click', event => {
     clearGrid();
-    
-    let i = 0;
-    let cell = "";
-    let container = "";
+  });
 
-    if (resize == true) {
-        grid = parseInt(prompt("Please enter a grid size."));
+  let btnResizeGrid = document.getElementById('resizeGrid');
+  btnResizeGrid.addEventListener('click', event => {
+    resizeGrid();
+  });
+
+  let btnMonochromeMode = document.getElementById('monochromeMode');
+  btnMonochromeMode.addEventListener('click', event => {
+    changeMode(color = false);
+    
+  });
+
+  let btnColorMode = document.getElementById('colorMode');
+  btnColorMode.addEventListener('click', event => {
+    changeMode(color = true);
+    
+  });
+
+})();
+
+function changeMode(color) {
+
+  if (color == false) {
+
+    allSquares = document.getElementById('grid-container').childNodes;
+
+    for (let i = 0; i < allSquares.length; i++) {
+      const element = allSquares[i];
+
+      element.removeAttribute('onmousover','monochromeMode(id)');
+      element.removeAttribute('onmousover','colorMode(id)');
+      element.removeAttribute('style');
+
+      element.setAttribute('onmouseover','monochromeMode(id)');
+      
     }
- 
-    do {
-      let cell = document.createElement('div');
-      let container = document.getElementById('grid-container');
-
-      cell.setAttribute('class', 'grid-item');
-      cell.setAttribute('id', i);
-
-      cell.setAttribute('onmouseover', 'colorCell(id)');
-      container.appendChild(cell);
-
-      container.style.gridTemplateColumns = `repeat(${grid}, 1fr)`;
-      container.style.gridTemplateRows = `repeat(${grid}, 1fr)`;
-
-      i++;
-    } while (i != grid * grid);
-
-    resize = false;
-  
     
+  } else if (color == true) {
+    allSquares = document.getElementById('grid-container').childNodes;
+
+    for (let i = 0; i < allSquares.length; i++) {
+      const element = allSquares[i];
+
+      element.removeAttribute('onmousover','monochromeMode(id)');
+      element.removeAttribute('style');
+
+      element.setAttribute('onmouseover','colorMode(id)');
+      
+    }
+    
+  }
+  
 }
 
-function colorCell(id) {
+// Handles coloring each cell black when the mouse hovers over.
+
+function monochromeMode(id) {
   let currentCell = document.getElementById(id.toString());
   currentCell.setAttribute('style', 'background-color: black;');
 }
 
-function clearGrid() {
+// Handles coloring each cell black when the mouse hovers over.
+
+function colorMode(id) {
+  let currentCell = document.getElementById(id.toString());
+
+  let randomColor =
+    '#' + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
+
+  currentCell.setAttribute('style', 'background-color: ' + randomColor + ';');
+}
+
+// Resizes the grid to user's choice.
+
+function resizeGrid() {
   let grid = document.getElementById('grid-container');
-  var range = document.createRange();
+  gridMultiplyer = parseInt(prompt('Please input a grid size e.g. 16 = 16x16'));
+
+  let i = 0;
+
+  let range = document.createRange();
   range.selectNodeContents(grid);
   range.deleteContents();
+
+  do {
+    let cell = document.createElement('div');
+    let container = document.getElementById('grid-container');
+
+    cell.setAttribute('class', 'grid-item');
+    cell.setAttribute('id', i);
+
+    cell.setAttribute('onmouseover', 'monochromeMode(id)');
+    container.appendChild(cell);
+
+    container.style.gridTemplateColumns = `repeat(${gridMultiplyer}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridMultiplyer}, 1fr)`;
+
+    i++;
+  } while (i != gridMultiplyer * gridMultiplyer);
+}
+
+// Redraws on-screen grid.
+
+function clearGrid() {
+  let grid = document.getElementById('grid-container');
+  let gridMultiplyer = Math.sqrt(grid.childNodes.length);
+
+  let i = 0;
+
+  let range = document.createRange();
+  range.selectNodeContents(grid);
+  range.deleteContents();
+
+  do {
+    let cell = document.createElement('div');
+    let container = document.getElementById('grid-container');
+
+    cell.setAttribute('class', 'grid-item');
+    cell.setAttribute('id', i);
+
+    cell.setAttribute('onmouseover', 'monochromeMode(id)');
+    container.appendChild(cell);
+
+    container.style.gridTemplateColumns = `repeat(${gridMultiplyer}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridMultiplyer}, 1fr)`;
+
+    i++;
+  } while (i != gridMultiplyer * gridMultiplyer);
 }
